7e07809a36a5db0ea8ac1447fad47f8ccb4f24d2
Peyton Sherwood <peytons@gmail.com>
2013-11-20 15:35:57 -0500

    alias stuff

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
 source ~/bin/git_svn_bash_prompt.sh
+source ~/.aliases
--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo Installing from $DIR
+ln -s $DIR/.gitconfig ~/.gitconfig
 ln -s $DIR/.bash_profile ~/.bash_profile
 ln -s $DIR/.bashrc ~/.bashrc
 ln -s $DIR/bin ~/bin
+ln -s $DIR/.aliases ~/.aliases
