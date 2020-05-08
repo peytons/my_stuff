5ba84746b6c40251ec4fb24b3b8ec5951ce61d46
Peyton Sherwood <peytons@gmail.com>
2020-05-08 15:20:59 -0500

    tracking vimrc

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ ln -s $DIR/.bash_profile ~/.bash_profile
 ln -s $DIR/.bashrc ~/.bashrc
 ln -s $DIR/bin ~/bin
 ln -s $DIR/.aliases ~/.aliases
+[[ ~/.vimrc -ef ./.vimrc ]] || echo "Renaming ~/.vimrc to ~/.vimrc.old" && mv -f ~/.vimrc ~/.vimrc.old
+ln $DIR/.vimrc ~/.vimrc
